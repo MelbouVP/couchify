6e2de4edaa281018c322e126c43aa7127523c528
MelbouVP <saurfanng@gmail.com>
2021-05-26 18:38:32 +0300

    Updated package scripts

--- react-ui/src/Redux/store.js
+++ react-ui/src/Redux/store.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 
-const middleWares = [thunk, logger];
+const middleWares = [thunk];
 
 const store = createStore(
     rootReducer,
